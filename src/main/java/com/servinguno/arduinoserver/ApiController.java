d113d12fd28c0b46b9036347e5f30ab9de49dff0
LOGAET <Xander.k990@gmail.com>
2024-04-23 16:18:11 +0300

    datetime

--- src/main/java/com/servinguno/arduinoserver/ApiController.java
+++ src/main/java/com/servinguno/arduinoserver/ApiController.java
@@ -1,12 +1,11 @@
 package com.servinguno.arduinoserver;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
@@ -16,9 +15,12 @@ public class ApiController {
     private final TemperatureRepository temperatureRepository;
 
     @PostMapping("/message")
-    public void getDataFromArduino(@RequestBody String data) {
+    public OffsetDateTime getDataFromArduino(@RequestBody String data) {
         System.out.println("Temp: " + data);
-        temperatureRepository.save(new Temperature(data, ZonedDateTime.now(
-                ZoneId.of("Europe/Moscow")).toOffsetDateTime()));
+        var date = ZonedDateTime.now(
+                ZoneId.of("Europe/Moscow")
+        ).toOffsetDateTime().plusHours(3);
+        temperatureRepository.save(new Temperature(data, date));
+        return date.minusHours(3);
     }
 }
